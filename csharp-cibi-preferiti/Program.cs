4db6690acd351f8355c9498965a372b8cc2136f6
Francesco-Rapetti <rapettif80@yahoo.it>
2024-04-16 16:00:57 +0200

    Add array logs

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // ESERCIZIO
+            string[] CibiPreferiti = { "Pasta", "Riso", "Pizza", "Panino", "Gelato" };
+            // lunghezza classifica
+            Console.WriteLine($"Lunghezza della classifica: {CibiPreferiti.Length}\n\n Classifica personale:");
+            // classifica
+            for (int i = 0; i < CibiPreferiti.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {CibiPreferiti[i]}");
+            }
+            // spaziatore
+            Console.WriteLine();
+            // cibo preferito
+            Console.WriteLine($"Cibo preferito: {CibiPreferiti[0]}");
+            // cibo meno preferito
+            Console.WriteLine($"Cibo meno preferito: {CibiPreferiti[CibiPreferiti.Length - 1]}");
+
+            // BONUS
+
         }
     }
 }
